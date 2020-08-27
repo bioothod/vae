31a8ad6f293ebc7085aaf2543bf8534ea0529d6f
Evgeniy Polyakov <zbr@ioremap.net>
2020-08-27 19:54:56 +0100

    Use padding like in the original resnet implementation instead of passing=same in conv layers

--- resnet.py
+++ resnet.py
@@ -1,38 +1,43 @@
 import tensorflow as tf
 
 relu_fn = tf.nn.relu
+global_bn_eps = 1.001e-5
 
 class BaseV2(tf.keras.layers.Layer):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.conv0 = tf.keras.layers.Conv2D(64, kernel_size=7, strides=2, use_bias=True, padding='same')
-        self.max_pooling0 = tf.keras.layers.MaxPool2D(pool_size=3, strides=2, padding='same')
+        self.pad0 = tf.keras.layers.ZeroPadding2D(padding=((3, 3), (3, 3)), name='conv1_pad')
+        self.conv0 = tf.keras.layers.Conv2D(64, kernel_size=7, strides=2, use_bias=True)
+
+        self.pad1 = tf.keras.layers.ZeroPadding2D(padding=((1, 1), (1, 1)), name='pool1_pad')
+        self.max_pooling0 = tf.keras.layers.MaxPool2D(pool_size=3, strides=2)
 
     def call(self, inputs, training):
-        x = self.conv0(inputs)
+        x = self.pad0(inputs)
+        x = self.conv0(x)
+
+        x = self.pad1(x)
         x = self.max_pooling0(x)
+
         return x
 
 class SmallBlockV2(tf.keras.layers.Layer):
     def __init__(self, channels_in, kernel_size=3, stride=1, conv_shortcut=False, **kwargs):
         super().__init__(**kwargs)
 
-        self.preact_bn = tf.keras.layers.BatchNormalization()
+        self.preact_bn = tf.keras.layers.BatchNormalization(epsilon=global_bn_eps)
 
-        self.bn0 = tf.keras.layers.BatchNormalization()
-        self.conv0 = tf.keras.layers.Conv2D(channels_in, kernel_size=3, strides=1, use_bias=False, padding='same')
+        self.bn0 = tf.keras.layers.BatchNormalization(epsilon=global_bn_eps)
+        self.conv0 = tf.keras.layers.Conv2D(channels_in, kernel_size=3, strides=1, use_bias=False)
 
-        self.bn1 = tf.keras.layers.BatchNormalization()
-        self.conv1 = tf.keras.layers.Conv2D(channels_in, kernel_size=kernel_size, strides=stride, use_bias=False, padding='same')
+        self.bn1 = tf.keras.layers.BatchNormalization(epsilon=global_bn_eps)
+        self.conv1 = tf.keras.layers.Conv2D(channels_in, kernel_size=kernel_size, strides=stride, use_bias=False)
 
         if conv_shortcut:
-            self.shortcut = tf.keras.layers.Conv2D(channels_out, kernel_size=1, strides=stride, padding='same')
+            self.shortcut = tf.keras.layers.Conv2D(channels_out, kernel_size=1, strides=stride)
         else:
-            if stride > 1:
-                self.shortcut = tf.keras.layers.MaxPool2D(pool_size=1, strides=stride, padding='same')
-            else:
-                self.shortcut = lambda x: x
+            self.shortcut = tf.keras.layers.MaxPool2D(pool_size=1, strides=stride)
 
     def call(self, inputs, training):
         x = self.preact_bn(inputs, training=training)
@@ -57,21 +62,22 @@ class BlockV2(tf.keras.layers.Layer):
         super().__init__(**kwargs)
 
         channels_out = channels_in * 4
-        self.preact_bn = tf.keras.layers.BatchNormalization()
+        self.preact_bn = tf.keras.layers.BatchNormalization(epsilon=global_bn_eps)
 
-        self.bn0 = tf.keras.layers.BatchNormalization()
-        self.conv0 = tf.keras.layers.Conv2D(channels_in, kernel_size=1, strides=1, use_bias=False, padding='same')
+        self.bn0 = tf.keras.layers.BatchNormalization(epsilon=global_bn_eps)
+        self.conv0 = tf.keras.layers.Conv2D(channels_in, kernel_size=1, strides=1, use_bias=False, name='conv0')
 
-        self.bn1 = tf.keras.layers.BatchNormalization()
-        self.conv1 = tf.keras.layers.Conv2D(channels_in, kernel_size=kernel_size, strides=stride, use_bias=False, padding='same')
+        self.pad1 = tf.keras.layers.ZeroPadding2D(padding=((1, 1), (1, 1)), name='conv_1_pad')
+        self.bn1 = tf.keras.layers.BatchNormalization(epsilon=global_bn_eps)
+        self.conv1 = tf.keras.layers.Conv2D(channels_in, kernel_size=kernel_size, strides=stride, use_bias=False)
 
-        self.conv2 = tf.keras.layers.Conv2D(channels_out, kernel_size=1, strides=1, padding='same')
+        self.conv2 = tf.keras.layers.Conv2D(channels_out, kernel_size=1, strides=1)
 
         if conv_shortcut:
-            self.shortcut = tf.keras.layers.Conv2D(channels_out, kernel_size=1, strides=stride, padding='same')
+            self.shortcut = tf.keras.layers.Conv2D(channels_out, kernel_size=1, strides=stride, name='shortcut')
         else:
             if stride > 1:
-                self.shortcut = tf.keras.layers.MaxPool2D(pool_size=1, strides=stride, padding='same')
+                self.shortcut = tf.keras.layers.MaxPool2D(pool_size=1, strides=stride)
             else:
                 self.shortcut = lambda x: x
 
@@ -85,6 +91,7 @@ class BlockV2(tf.keras.layers.Layer):
         x = self.bn0(x, training=training)
         x = relu_fn(x)
 
+        x = self.pad1(x)
         x = self.conv1(x)
         x = self.bn1(x, training=training)
         x = relu_fn(x)
